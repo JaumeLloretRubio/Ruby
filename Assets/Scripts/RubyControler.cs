a157255251992dca4b1ded7fe2ba69f8b5bdc8d6
JaumeLloretRubio <al425988@uji.es>
2022-11-17 18:35:27 +0100

    17/11/22

--- Assets/Scripts/RubyControler.cs
+++ Assets/Scripts/RubyControler.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class RubyControler : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
     Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
@@ -11,6 +21,7 @@ public class RubyControler : MonoBehaviour
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 10;
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -24,5 +35,26 @@ public class RubyControler : MonoBehaviour
         position.y = position.y + 3.0f * vertical * Time.deltaTime ;
 
         rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+    }
+    
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
